dee7bf53256d3eae6e39c3ec48410a6a9cdd57b3
jenir <A00262844@mycambrian.ca>
2024-02-24 15:52:37 -0500

    Working on login screen

--- SignIn.xaml.cs
+++ SignIn.xaml.cs
@@ -1,10 +1,29 @@
-
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
-
 namespace BudgetBuddy;
 
 public partial class SignIn : ContentPage
 {
-    
+    public SignIn()
+    {
+        InitializeComponent();
+    }
+    bool eye = false;
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        ImageButton statusb = (ImageButton)sender;
+        if (!eye)
+        {
+            statusb.Source = "https://cdn0.iconfinder.com/data/icons/typicons-2/24/lock-open-outline-1024.png";
+            //Password.IsPassword = false;
+            eye = true;
+        }
+        else
+        {
+            statusb.Source = "https://cdn0.iconfinder.com/data/icons/typicons-2/24/lock-closed-outline-512.png";
+            //Password.IsPassword = true;
+            eye = false;
+
+        }
+       
+    }
 }
