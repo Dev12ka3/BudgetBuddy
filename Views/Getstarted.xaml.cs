9d22e5d32fc5e8cd6e821a6fd964efa38757e5de
jenir <A00262844@mycambrian.ca>
2024-03-07 18:06:45 -0500

    saprated diffrent view finilized the main window to prompt user

---
+++ Views/Getstarted.xaml.cs
@@ -0,0 +1,26 @@
+using BudgetBuddy.Views;
+using Microsoft.Maui;
+
+
+
+namespace BudgetBuddy;
+
+public partial class Getstarted : ContentPage
+{
+    public Getstarted()
+    { 
+        InitializeComponent();
+    }
+
+    private void signup(object sender, EventArgs e)
+    {
+        signup(sender, e);
+    }
+    private void Login(object sender, EventArgs e)
+    {
+        
+
+
+    }
+
+}
